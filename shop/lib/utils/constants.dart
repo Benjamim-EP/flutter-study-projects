40a1d124ef04d11676d43f5a334a753df2df097a
Benjamim-EP <bannk18@gmail.com>
2023-01-05 17:02:37 -0300

    Registrar usuario

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -3,4 +3,5 @@ class Constants {
       'https://shop-48117-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
       'https://shop-48117-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyDzEYFq728FmnfQQuMqSnXoubkQADTi8UU';
 }
