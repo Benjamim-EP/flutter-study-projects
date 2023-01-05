a87ac5eea08e32ae4bf6044db3afe5954c54e368
Benjamim-EP <bannk18@gmail.com>
2023-01-05 13:11:01 -0300

    Armazenamento de pedidos no firebase

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-48117-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-48117-default-rtdb.firebaseio.com/orders';
 }
