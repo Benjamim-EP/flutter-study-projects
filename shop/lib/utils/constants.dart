af30b4865df84f06dbd0e2e25e4da38a97d4e202
Benjamim-EP <bannk18@gmail.com>
2023-01-05 21:47:14 -0300

    Associar favorito ao usuario

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -1,7 +1,10 @@
 class Constants {
+  static const userFavoritesUrl =
+      'https://shop-48117-default-rtdb.firebaseio.com/userFavorites';
   static const productBaseUrl =
       'https://shop-48117-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
       'https://shop-48117-default-rtdb.firebaseio.com/orders';
+
   static const webApiKey = 'AIzaSyDzEYFq728FmnfQQuMqSnXoubkQADTi8UU';
 }
